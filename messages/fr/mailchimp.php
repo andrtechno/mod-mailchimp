a38ef39c5880c7a674876b3f32a6bbe01acf48ee
Andrew <andrew.panix@gmail.com>
2020-04-02 20:48:05 +0300

    init

--- messages/fr/mailchimp.php
+++ messages/fr/mailchimp.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * @copyright Copyright &copy; Gogodigital Srls
- * @company Gogodigital Srls - Wide ICT Solutions
- * @website http://www.gogodigital.it
- * @github https://github.com/cinghie/yii2-mailchimp
- * @license BSD 3-Clause
- * @package yii2-mailchimp
- * @version 0.2.2
- */
-
 return [
     'Email' => 'Email',
     'First Name' => 'Prénom',
